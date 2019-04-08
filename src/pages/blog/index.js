e6093cc8f15e3f8afa1b31cabd6f257be4d65bfd
shanqyeet <shanqyeet@gmail.com>
2019-04-08 11:55:30 +0800

    70% completed index page design

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,38 +1,26 @@
 import React from 'react'
-
+import PropTypes from 'prop-types'
+import { Link, graphql, StaticQuery } from 'gatsby'
 import Layout from '../../components/Layout'
 import BlogRoll from '../../components/BlogRoll'
+import Navbar from '../../components/Navbar'
 
 export default class BlogIndexPage extends React.Component {
   render() {
+    const { data } = this.props
+    const { edges: posts } = data.allMarkdownRemark
+
     return (
       <Layout>
-        <section className="section">
-          <div className="container">
-            <div className="content">
-              <div
-                className="full-width-image-container margin-top-0"
-                style={{
-                  backgroundImage: `url('/img/blog-index.jpg')`,
-                }}
-              >
-                <h1
-                  className="has-text-weight-bold is-size-1"
-                  style={{
-                    boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
-                    backgroundColor: '#f40',
-                    color: 'white',
-                    padding: '1rem',
-                  }}
-                >
-                  Latest Stories
-                </h1>
-              </div>
-            </div>
-            <BlogRoll />
+        <Navbar  hasLightBg currentPage='blog-index'/>
+        <section className="blog-index">
+          <div className="title-container">
+            <h1 className="title gold-color f-arnopro-b">LATEST STORIES</h1>
           </div>
-        </section>
+          <BlogRoll />
+       </section>
       </Layout>
     )
   }
 }
+
