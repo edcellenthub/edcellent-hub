b3bfe89917c65de4ff4101fe4274bd59049efe62
Loo Jye Miin <loojyemiin@gmail.com>
2019-04-13 14:35:14 +0800

    content index page

--- src/pages/content/index.js
+++ src/pages/content/index.js
@@ -5,16 +5,63 @@ import Layout from '../../components/Layout'
 import Navbar from '../../components/Navbar'
 import Contact from '../../components/Contact'
 
-import { ContentPageTemplate } from '../../templates/content-page';
+import { ContentPage } from '../../templates/content-page';
 
 
-export default class ContentPage extends React.Component {
-  
+export default class Content extends React.Component {
+  state = {
+    expand: false
+  }
+
+  toggleExpandTR = () => {
+    this.setState({ expand: !this.state.expand })
+  }
+
+  toggleExpandCTR = () => {
+    this.setState({ expand: !this.state.expand })
+  }
+
   render() {
+    const { expand } = this.state
     return (
       <Layout>
-        <Navbar  hasLightBg currentPage='blog'/>
-        
+        <Navbar  hasLightBg currentPage='content'/>
+        <section className="section">
+          <div id="content-index">
+          <h2 className="f-arnopro-s text-medium dark-gold margin-bottom-2">
+          The text we offer students in 2019 includes
+          </h2>
+            <div onClick={this.toggleExpandTR} className="content-box-1" >
+              <div className="overlay">
+                <div className="border">
+                <h3 className="f-arnopro-s text-md-md">TEXT RESPONSE</h3>
+                </div>  
+              </div> 
+            </div>
+            <div onClick={this.toggleExpandCTR} className="content-box-2" >
+              <div className="overlay">
+                <div className="border">
+                <h3 className="f-arnopro-s text-md-md">COMPRATIVE TEXT RESPONSE</h3>
+                </div>  
+              </div>        
+            </div>
+            <div className="content-box-3" >
+              <div className="overlay">
+                <div className="border">
+                <h3 className="f-arnopro-s text-md-md">SPEECHES WRITING MASTER CLASS</h3>
+                </div>  
+              </div> 
+            </div>
+            <div className="content-box-4" >
+              <div className="overlay">
+                <div className="border">
+                <h3 className="f-arnopro-s text-md-md">ANALYSING ARGUMENTS</h3>
+                </div>  
+              </div>        
+            </div>
+            
+          </div>
+          </section>
         <Contact />
       </Layout>
     )
