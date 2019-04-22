b993c5bff9c08399d5397297b731b08e0047fcf2
Loo Jye Miin <loojyemiin@gmail.com>
2019-04-22 15:44:17 +0800

    added line break at title

--- src/components/TestimonialVid.js
+++ src/components/TestimonialVid.js
@@ -37,7 +37,7 @@ export default class TestimonialVideo extends React.Component {
             style={{ display: isPlaying ? 'none' : 'block' }}
             className="landing-top-banner-info-box text-center f-arnopro-r"
           >
-          <div className="f-arnopro-s testimonial-title">Hear what our students have to say about us</div>          
+          <div className="f-arnopro-s testimonial-title">Hear what our students<br />have to say about us</div>          
           <button className="play-video-btn" onClick={this.playVideo}>
             <span>WATCH VIDEO</span>
             <i class="fas fa-caret-right" />
