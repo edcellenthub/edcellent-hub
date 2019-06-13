cad886ff3f1ebdd8d3f6f61e6649f1be9ccd5bdf
Liren <liren90@gmail.com>
2019-06-13 18:22:34 +0800

    Remove gtm code

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -11,11 +11,6 @@ const TemplateWrapper = ({ children }) => {
     <div>
       <Helmet>
         <html lang="en" />
-        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-K4MT57K');</script>
         <title>{title}</title>
         <meta name="description" content={description} />
         <link
@@ -46,8 +41,6 @@ const TemplateWrapper = ({ children }) => {
         <meta property="og:url" content="/" />
         <meta property="og:image" content="/img/edcellent_home.jpg" />
       </Helmet>
-      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K4MT57K"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
       {/* <Navbar /> */}
       <div className="f-nunito">{children}</div>
       <Footer />
